c92d3cbc6b4c00f03d6e8ca7f230a49aabdfe36d
Michael Henrique <oliveirasantos.mh@gmail.com>
2021-11-11 19:29:24 -0300

    test: testing ifstream lib

--- src/ReadFile/readFile.cpp
+++ src/ReadFile/readFile.cpp
@@ -5,22 +5,21 @@ namespace powerflow {
 ReadFile::ReadFile(std::string path) : input_file_{std::ifstream{path}} {}
 
 std::vector<std::string> ReadFile::get_content() {
+  auto result = std::vector<std::string>{};
   if (input_file_.is_open()) {
-    auto result = std::vector<std::string>{};
     auto string_buffer = std::string{};
     while (std::getline(input_file_, string_buffer)) {
-      auto stream_buffer = std::stringstream{string_buffer};
-      auto temp_string = std::string{};
-      stream_buffer >> temp_string;
+      //   auto stream_buffer = std::stringstream{string_buffer};
+      //   auto temp_string = std::string{};
+      //   stream_buffer >> temp_string;
 
-      result.emplace_back(temp_string);
+      result.emplace_back(string_buffer);
     }
-
-    return result;
 
   } else {
     std::cerr << "Cant open file\n";
   }
+  return result;
 }
 
 }  // namespace powerflow
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 
 #include "ReadFile/readFile.hpp"
 int main() {
-  std::cout << "Hello, read-file\n";
-  return 0;
+  auto file = powerflow::ReadFile("./content.txt");
+
+  for (auto const& content : file.get_content()) {
+    std::cout << content << "\n";
+  }
+  return EXIT_SUCCESS;
 }
