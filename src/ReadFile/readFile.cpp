cbeb2156a991a59be45584c179725ac1c19bba52
Michael Henrique <oliveirasantos.mh@gmail.com>
2021-11-11 18:07:54 -0300

    feat: reading file

--- src/ReadFile/readFile.cpp
+++ src/ReadFile/readFile.cpp
@@ -1,1 +1,26 @@
 #include "readFile.hpp"
+
+namespace powerflow {
+
+ReadFile::ReadFile(std::string path) : input_file_{std::ifstream{path}} {}
+
+std::vector<std::string> ReadFile::get_content() {
+  if (input_file_.is_open()) {
+    auto result = std::vector<std::string>{};
+    auto string_buffer = std::string{};
+    while (std::getline(input_file_, string_buffer)) {
+      auto stream_buffer = std::stringstream{string_buffer};
+      auto temp_string = std::string{};
+      stream_buffer >> temp_string;
+
+      result.emplace_back(temp_string);
+    }
+
+    return result;
+
+  } else {
+    std::cerr << "Cant open file\n";
+  }
+}
+
+}  // namespace powerflow
--- src/ReadFile/readFile.hpp
+++ src/ReadFile/readFile.hpp
@@ -1,8 +1,19 @@
 #ifndef READ_FILE_HPP
 #define READ_FILE_HPP
 
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
 namespace powerflow {
-class ReadFile {};
+class ReadFile {
+ public:
+  ReadFile(std::string path);
+  std::vector<std::string> get_content();
+
+ private:
+  std::ifstream input_file_{};
+};
 }  // namespace powerflow
 
 #endif
